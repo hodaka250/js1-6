1f0bc7d28577df4e0155ec36a5a673bb4d876f76
hodaka250 <95903411+hodaka250@users.noreply.github.com>
2023-04-23 01:45:33 +0900

    課題提出1-6
    配列

---
+++ kadai6.js
@@ -0,0 +1,16 @@
+let score=[10,15,20,25];
+console.log(score);
+
+for(let i = 0; i < score.length; i++){
+    if(score[i]%2===0){
+        console.log(score[i]+'は偶数です');
+    }
+}
+
+let car={
+    gass:20.5,
+    num:1234,
+};
+console.log(car);
+console.log('ガソリンは'+car.gass+'です');
+console.log('ナンバーは'+car.gass+'です');
